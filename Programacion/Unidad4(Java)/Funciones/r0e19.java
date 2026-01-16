2f78481980825deaef622df2734001690d2cefbf
carmonalarajuan001 <carmonalarajuan001.alu@stemgranada.com>
2026-01-16 08:51:26 +0100

    Ejercicio 19 y 20 empezado

--- Programacion/Unidad4(Java)/Funciones/r0e19.java
+++ Programacion/Unidad4(Java)/Funciones/r0e19.java
@@ -1,8 +1,11 @@
 
 public class r0e19 {
-    public static String capitalizarPrimeraLetra(String palabra){
-        
-       
-        
+    public static String capitalizarPrimeraLetra(String palabra) {
+        return Character.toUpperCase(palabra.charAt(0)) + palabra.substring(1);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(capitalizarPrimeraLetra("hola"));
     }
 }
---
+++ Programacion/Unidad4(Java)/Funciones/r0e20.java
@@ -0,0 +1,3 @@
+public class r0e20 {
+    
+}
